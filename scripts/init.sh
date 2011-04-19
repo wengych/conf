67a6f4274914b737861c6c628bccf42df89014b1
Charles Weng <wengych@gmail.com>
2011-04-19 22:24:53 +0800

    Add vim and git configuration file.

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,27 @@ then
   rm ${HOME}/.bash_aliases
 fi
 
-
 echo "create softlink for .bash_aliases"
 ln -s $PWD/bash-conf/bash_aliases ~/.bash_aliases
 
+
+
+if [ -f ${HOME}/.vimrc ]
+then
+  echo ".vimrc file exist, delete it now."
+  rm ${HOME}/.vimrc
+fi
+
+echo "create softlink for .vimrc"
+ln -s $PWD/vim/vimrc ~/.vimrc
+
+
+
+if [ -f ${HOME}/.gitconfig ]
+then
+  echo ".gitconfig file exist, delete it now."
+  rm ${HOME}/.gitconfig
+fi
+
+echo "create softlink for .gitconfig"
+ln -s $PWD/git/gitconfig ~/.gitconfig
